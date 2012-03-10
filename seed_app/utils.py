2f372b200418fb834718fe1cdd99e44887a66398
Steve Pulec <steve@yipit.com>
2012-03-10 13:44:47 -0500

    working

--- seed_app/utils.py
+++ seed_app/utils.py
@@ -1,2 +1,5 @@
+def model_namespace(model_instance):
+    return "%s__%s" % (model_instance._meta.app_label, model_instance._meta.module_name)
+
 def queryset_namespace(queryset):
-    return "%s.%s" % (queryset.model._meta.app_label, queryset.model._meta.module_name)
+    return "%s__%s" % (queryset.model._meta.app_label, queryset.model._meta.module_name)
