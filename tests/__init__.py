674da1834f1b149eb69d2df4f1117bc28bbff652
Lucas Carvalho <lucascarvalho@gmail.com>
2017-11-10 14:10:24 -0500

    Fix tests

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import django
+
+# test_dir = os.path.dirname(os.path.abspath(__file__))
+# # sys.path.insert(0, os.path.join(test_dir, os.path.pardir))
+# sys.path.insert(0, test_dir)
+django.setup()
